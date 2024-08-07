853a93f6bb08b729f66719b651b30d4d65ed6678
hangxigood <hangxigood@gmail.com>
2024-08-06 20:16:56 -0600

    Lab Assignment 4: Event Handling & State Lifting

--- ToDoForm.jsx
+++ ToDoForm.jsx
@@ -1,15 +1,22 @@
 // ToDoForm.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 
-const ToDoForm = () => {
+function ToDoForm({ addTask }) {
+  const [taskText, setTaskText] = useState('');
+
   return (
     <View style={styles.form}>
       <TextInput
         style={styles.input}
         placeholder="Add a new task..."
+        onChangeText={(text) => setTaskText(text)}
+        value={taskText}
       />
-      <Button title="Add" />
+      <Button title="Add Task" onPress={() => {
+        addTask(taskText);
+        setTaskText('');
+      }} />
     </View>
   );
 }
